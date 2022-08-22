c78471549e112de37d116c56e7f44727fd07a1cf
LingSha <LingSha@qq.com>
2022-08-22 22:01:16 +0800

    test version 1.0
    Signed-off-by: LingSha <LingSha@qq.com>

---
+++ src/main/java/com/lingsha/helloworld.java
@@ -0,0 +1,17 @@
+package com.lingsha;
+
+/**
+ * @auther Ling.Sha
+ * @date 2022/8/22 - 21:05
+ */
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("hello , git!");
+    }
+
+    public void test()
+    {
+
+    }
+}
